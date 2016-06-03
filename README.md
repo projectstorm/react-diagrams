b0e48f3ad97a271e73f0eac6e2e31841200cbaca
Dylan Vorster <dylan@eezipay.com>
2016-06-03 11:38:54 +0200

    Added some readme stuffs

---
+++ README.md
@@ -0,0 +1,6 @@
+# STORM React Diagrams
+
+This library makes it easy to create interactive flow diagrams using react. 
+You can create custom elements as long as they contain port multiple PortWidgets.
+
+![Demo](./demo.gif)
--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/**
+ * @author Dylan Vorster
+ */
 module.exports = {
-	
+	Engine: require("./Engine"),
+	CanvasWidget: require("./widgets/CanvasWidget"),
+	PortWidget: require("./widgets/PortWidget"),
+	BasicNodeWidget: require("./widgets/BasicNodeWidget"),
 };
