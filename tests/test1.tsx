9523d641131c6f31cd76ddf568608b466ceac599
Dylan Vorster <dylanvorster.dv@gmail.com>
2017-11-20 23:35:33 +0200

    first tests

--- tests/test1.tsx
+++ tests/test1.tsx
@@ -1,11 +1,42 @@
 import "jest";
 import * as puppeteer from "puppeteer"
 
-it('should load the diagram with no issues', async () => {
-	let browser = await puppeteer.launch({
-		headless: true
+var browser;
+let index = 0;
+
+async function itShould(directive, test: (page: puppeteer.Page) => any){
+	it(directive, async () => {
+		let page = await browser.newPage();
+		await page.goto('file://' + __dirname + '/../.out/index.html');
+		await test(page);
+		await page.close();
 	});
-	let page = await browser.newPage();
-	await page.goto('file://'+__dirname+'/../.out/index.html');
-	await browser.close();
+}
+
+beforeAll(async () => {
+	browser = await puppeteer.launch({
+		headless: false
+	});
 });
+
+afterAll(() => {
+	browser.close();
+});
+
+
+describe("simple test", async () => {
+
+	itShould('should drag a new link', async (page) => {
+		await page.mouse.move(410, 152);
+		await page.mouse.down();
+		await page.mouse.move(610, 352);
+		await page.mouse.up();
+	});
+
+	itShould('should drag a node', async (page) => {
+		await page.mouse.move(390, 145);
+		await page.mouse.down();
+		await page.mouse.move(300, 300);
+		await page.mouse.up();
+	});
+})
