e68e1474eaf576b31317e0df9c2059b1ae858ff4
Dylan Vorster <dylanvorster.dv@gmail.com>
2017-11-20 21:27:17 +0200

    use headless

--- tests/test1.tsx
+++ tests/test1.tsx
@@ -3,7 +3,7 @@ import * as puppeteer from "puppeteer"
 
 it('should load the diagram with no issues', async () => {
 	let browser = await puppeteer.launch({
-		headless: false
+		headless: true
 	});
 	let page = await browser.newPage();
 	await page.goto('file://'+__dirname+'/../.out/index.html');
