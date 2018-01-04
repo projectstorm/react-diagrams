2f048d6a94cbd1f3d34b9704ee727ef471f6fa49
Dylan Vorster <dylanvorster.dv@gmail.com>
2018-01-04 23:26:54 +0200

    trying to fix jest

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,11 @@
 module.exports = {
 	verbose: true,
 	moduleFileExtensions: ["ts", "tsx", "js", "json"],
+	transformIgnorePatterns: ["/node_modules/"],
 	transform: {
 		"^.+\\.(ts|tsx)$": "./tests/helpers/tsx-preprocessor.js",
-		"^.+\\.(scss)$": "./tests/helpers/scss-preprocessor.js"
+		"^.+\\.(scss)$": "./tests/helpers/scss-preprocessor.js",
+		"^.+\\.css$": "./tests/helpers/scss-preprocessor.js"
 	},
 	"testMatch": [
 		"**/tests/*\.test\.*"
