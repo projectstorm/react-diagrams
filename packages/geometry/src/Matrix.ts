1e95edbc6fbfd423297c17d53bcdb40713a4ce9f
Dylan Vorster <dylanvorster.dv@gmail.com>
2020-09-23 10:24:36 +0200

    pretty

--- packages/geometry/src/Matrix.ts
+++ packages/geometry/src/Matrix.ts
@@ -5,23 +5,18 @@ export class Matrix {
 		this.matrix = matrix;
 	}
 
-	mmul = (matrix: Matrix): Matrix => {
-		// prettier-ignore
+	mmul(matrix: Matrix): Matrix {
 		this.matrix = this.matrix.map((row, i) =>
-				matrix.asArray()[0].map((_, j) =>
-					row.reduce((acc, _, n) =>
-						acc + this.matrix[i][n] * matrix.asArray()[n][j], 0
-					)
-				)
-			);
+			matrix.asArray()[0].map((_, j) => row.reduce((acc, _, n) => acc + this.matrix[i][n] * matrix.asArray()[n][j], 0))
+		);
 		return this;
-	};
+	}
 
-	asArray = (): number[][] => {
+	asArray(): number[][] {
 		return this.matrix;
-	};
+	}
 
-	get = (rowIndex: number, columnIndex: number): number => {
+	get(rowIndex: number, columnIndex: number): number {
 		return this.asArray()[rowIndex][columnIndex];
-	};
+	}
 }
