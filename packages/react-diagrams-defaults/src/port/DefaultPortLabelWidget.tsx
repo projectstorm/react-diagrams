9b58fa39634d2664d3e67e1496e9344d6b8a5ae6
artturik <arturbabyuk@gmail.com>
2020-10-12 23:52:13 +0300

    fix port background color
    rgba(white, 0.1) is not valid css, changed it to valid
    rgba syntax - <rgba()> = rgba( <percentage>{3} [ / <alpha-value> ]? ) | rgba( <number>{3} [ / <alpha-value> ]? ) from https://developer.mozilla.org/en-US/docs/Web/CSS/background-color

--- packages/react-diagrams-defaults/src/port/DefaultPortLabelWidget.tsx
+++ packages/react-diagrams-defaults/src/port/DefaultPortLabelWidget.tsx
@@ -23,7 +23,7 @@ namespace S {
 	export const Port = styled.div`
 		width: 15px;
 		height: 15px;
-		background: rgba(white, 0.1);
+		background: rgba(255, 255, 255, 0.1);
 
 		&:hover {
 			background: rgb(192, 255, 0);
