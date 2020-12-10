38365f9b33a55766d27914680e1bb08f518d5ddf
Daniel Lobo <danlobo@gmail.com>
2020-12-10 09:33:07 -0300

    Fix SSR issues (#75, #315)
    Replace closest package with polyfill
    Fix webpack4 window issue (https://stackoverflow.com/questions/55849240/window-is-not-defined-after-a-build-with-webpack)

--- packages/react-canvas-core/src/Toolkit.ts
+++ packages/react-canvas-core/src/Toolkit.ts
@@ -24,9 +24,18 @@ export class Toolkit {
 	 * Finds the closest element as a polyfill
 	 */
 	public static closest(element: Element, selector: string) {
-		if (document.body.closest) {
-			return element.closest(selector);
+		if (Element && !Element.prototype.closest) {
+			Element.prototype.closest = function (s) {
+				var el = this;
+
+				do {
+					if (Element.prototype.matches.call(el, s)) return el;
+					el = el.parentElement || el.parentNode;
+				} while (el !== null && el.nodeType === 1);
+				return null;
+			};
 		}
-		return closest(element, selector);
+
+		return element.closest(selector);
 	}
 }
