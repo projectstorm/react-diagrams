4f3df60758606e5dfcdc807063e7b27dd01cfb58
Dylan Vorster <dylanvorster.dv@gmail.com>
2021-03-12 23:55:45 +0200

    fixes

--- diagrams-demo-gallery/.storybook/manager.js
+++ diagrams-demo-gallery/.storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/addons';
-import { themes } from '@storybook/theming';
+import diagramsTheme from "./theme";
 
 addons.setConfig({
-	theme: themes.dark,
+	theme: diagramsTheme,
 });
