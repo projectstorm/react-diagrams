d2a197245b87f2002df802e06db861dd99f9220b
Dylan Vorster <dylanvorster.dv@gmail.com>
2023-01-23 20:32:57 -0700

    pls3

--- diagrams-demo-gallery/jest-puppeteer.config.js
+++ diagrams-demo-gallery/jest-puppeteer.config.js
@@ -1,8 +1,5 @@
 module.exports = {
 	launch: {
-		args: ['--no-sandbox'],
-		executablePath: process.env.PUPPETEER_EXEC_PATH || undefined, // set by docker container
-		headless: false
-	},
-	browserContext: 'default'
+		browserWSEndpoint: 'ws://localhost:3000'
+	}
 };
