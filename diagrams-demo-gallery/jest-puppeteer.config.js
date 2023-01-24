95b35d67782ecfc8c42b0910bdeef9468a0835f9
Dylan Vorster <dylanvorster.dv@gmail.com>
2023-01-23 20:47:36 -0700

    get rid of puppeteer

--- diagrams-demo-gallery/jest-puppeteer.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	launch: {
-		browserWSEndpoint: 'ws://localhost:3000'
-	}
-};
--- diagrams-demo-gallery/jest.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-module.exports = {
-	preset: 'jest-puppeteer',
-	transform: {
-		'^.+\\.tsx?$': 'ts-jest'
-	},
-	roots: [path.join(__dirname, 'tests-e2e')],
-	testMatch: ['**/*.test.{ts,tsx}']
-};
