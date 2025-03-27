d8284aa8a4fe31d93a85a39947af24983b80de37
Daniel Spindler <daniel.spindler@braun-its.de>
2025-03-27 14:06:15 +0100

    Fixed formatting

--- diagrams-demo-gallery/.storybook/main.js
+++ diagrams-demo-gallery/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
 	stories: ['../demos/*.stories.tsx'],
-	addons: ['@storybook/addon-actions', "@storybook/addon-webpack5-compiler-babel"],
+	addons: ['@storybook/addon-actions', '@storybook/addon-webpack5-compiler-babel'],
 	framework: '@storybook/react-webpack5'
 };
